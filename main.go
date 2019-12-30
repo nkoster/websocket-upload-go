daf180068c8c16c32924d512e5989352310dd18d
Niels Koster <n.koster@portavita.eu>
2019-12-30 23:04:41 +0100

    Web Socket Reader

--- main.go
+++ main.go
@@ -17,13 +17,33 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
 }
 
+func reader(conn *websocket.Conn) {
+	for {
+
+		messageType, p, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-	_, err := upgrader.Upgrade(w, r, nil)
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 	}
 	log.Println("Connection!")
+	reader(ws)
 }
 
 func setupRoutes() {
