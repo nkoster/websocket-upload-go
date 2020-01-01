5a23005d5ffcdfbeeb16b4c6b40378ae2e7bf8de
Niels Koster <n.koster@portavita.eu>
2020-01-02 00:19:08 +0100

    New approach

--- main.go
+++ main.go
@@ -2,57 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-
-		messageType, p, err := conn.ReadMessage()
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		log.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-
-	}
-}
-
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("Connection!")
-	reader(ws)
-}
-
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
-}
+var upgrader = websocket.Upgrader{}
 
 func main() {
-	fmt.Println("Hellow Orld")
-	setupRoutes()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("aap")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Connection...")
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			for {
+				mt, _, _ := conn.ReadMessage()
+				fmt.Println(mt)
+			}
+		}(conn)
+	})
+	http.ListenAndServe(":8086", nil)
 }
